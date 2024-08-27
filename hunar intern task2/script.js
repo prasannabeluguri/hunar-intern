cee83bda032b6a59c66183793ac0e4698bec44bf
prasannabeluguri <prasannakumarbeluguri@gmail.com>
2024-08-27 20:04:29 +0530

    Add files via upload

---
+++ hunar intern task2/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav ul li a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const href = this.getAttribute('href');
+            window.location.href = href;
+        });
+    });
+});
